3d389d0ae417df77e11d35e78be5e89f8d887d67
NormandM <nmartin1956@gmail.com>
2020-03-29 16:52:09 -0400

    Initial Commit

---
+++ HistoryCards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HistoryCards
+//
+//  Created by Normand Martin on 2020-03-29.
+//  Copyright © 2020 Normand Martin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
